1f7bf6719d7281aa99c1fbd4dab0dcf8c8a8a0b2
Pavel Klyukin <klukin0202@yandex.ru>
2025-02-19 00:54:28 +0300

    Function for replacing double slashes to single slash. Modified urljoin.

--- vaultx/utils.py
+++ vaultx/utils.py
@@ -13,3 +13,26 @@ def get_token_from_env() -> Optional[str]:
             with open(token_file_path) as f_in:
                 token = f_in.read().strip()
     return token
+
+
+def urljoin(*args: str) -> str:
+    """
+    Joins given arguments into url. Trailing and leading slashes are stripped for each argument.
+    :param args: Multiple parts of a URL to be combined into one string.
+    :return: Full URL combining all provided arguments
+    """
+
+    return "/".join(str(x).strip("/") for x in args)
+
+
+def replace_double_slashes_to_single(url: str) -> str:
+    """
+    Vault CLI treats a double forward slash ('//') as a single forward slash for a given path.
+    To avoid issues with the requests module's redirection logic, we perform the same translation here.
+    :param url: URL as a string
+    :return: Modified URL
+    """
+
+    while "//" in url:
+        url = url.replace("//", "/")
+    return url

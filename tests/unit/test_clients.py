d8567f2c19a3e6dd2f1028f440e6e7d66c810678
Pavel Klyukin <klukin0202@yandex.ru>
2025-03-05 15:01:57 +0300

    ✨ Async client implementation (#47)
    * Async Audit implementation
    
    * Async Auth implementation
    
    * Async Capabilities implementation
    
    * Async Health implementation
    
    * Async Init implementation
    
    * Async Key implementation
    
    * Async Leader implementation
    
    * Async Lease implementation
    
    * Async Mount implementation
    
    * Async Namespace implementation
    
    * Async Policies implementation
    
    * Async Policy implementation
    
    * Async Quota implementation
    
    * Async Raft implementation
    
    * Async Seal implementation
    
    * Async Wrapping implementation
    
    * Unit tests for Async Audit provided
    
    * Unit tests for Async Auth provided
    
    * Unit tests for Async Capabilities provided
    
    * Unit tests for Async Health provided
    
    * Unit tests for Async Init provided
    
    * Unit tests for Async Key provided
    
    * Unit tests for Async Leader provided
    
    * Unit tests for Async Lease provided
    
    * Unit tests for Async Mount provided
    
    * Unit tests for Async Namespace provided
    
    * Unit tests for Async Policies provided
    
    * Unit tests for Async Policy provided
    
    * Unit tests for Async Quota provided
    
    * Unit tests for Async Raft provided
    
    * Unit tests for Async Seal provided
    
    * Unit tests for Async Wrapping provided
    
    * Initialize AsyncSystemBackend for the sake of AsyncClient
    
    * AiohttpTransport, AsyncAdapter, AsyncRawAdapter and AsyncJsonAdapter provided
    
    * AsyncClient class implementation provided
    
    * Provided tests for async instances of Auth Methods
    
    * Added tests for async adapters
    
    * Async Client class implementation provided
    
    * Unit tests for async system backend provided
    
    * api initialization for the sake of client's use
    
    * import CLient and AsyncClient to use in prod
    
    * Code style changes

--- tests/unit/test_clients.py
+++ tests/unit/test_clients.py
@@ -1,7 +1,7 @@
-from unittest import TestCase
+from unittest import IsolatedAsyncioTestCase, TestCase
 
-from vaultx import Client
-from vaultx.adapters import JsonAdapter
+from vaultx import AsyncClient, Client
+from vaultx.adapters import AsyncJsonAdapter, JsonAdapter
 
 
 class TestClient(TestCase):
@@ -12,6 +12,22 @@ class TestClient(TestCase):
         old_adapter = client.adapter
 
         client.adapter = JsonAdapter()
+
+        self.assertIsNot(client.adapter, old_adapter)
+        self.assertSetEqual(
+            {client.adapter},
+            {client.secrets.adapter, client.sys.adapter, client.auth.adapter},
+        )
+
+
+class TestAsyncClient(IsolatedAsyncioTestCase):
+    """Unit tests providing coverage for async_client related methods."""
+
+    async def test_setting_adapter_on_client_sets_adapter_of_endpoint_classes(self):
+        client = AsyncClient()
+        old_adapter = client.adapter
+
+        client.adapter = AsyncJsonAdapter()
 
         self.assertIsNot(client.adapter, old_adapter)
         self.assertSetEqual(

5b6ae527acf8456c1f863a8e78437e56ec9399d0
Pavel Klyukin <klukin0202@yandex.ru>
2025-03-15 21:57:25 +0300

    Ssh secrets engine provided for the client usage

--- vaultx/api/async_secrets_engines/__init__.py
+++ vaultx/api/async_secrets_engines/__init__.py
@@ -17,6 +17,7 @@ from vaultx.api.async_secrets_engines.kv_v2 import KvV2
 from vaultx.api.async_secrets_engines.ldap import Ldap
 from vaultx.api.async_secrets_engines.pki import Pki
 from vaultx.api.async_secrets_engines.rabbitmq import RabbitMQ
+from vaultx.api.async_secrets_engines.ssh import Ssh
 from vaultx.api.vault_api_base import AsyncVaultApiBase
 
 
@@ -34,6 +35,7 @@ __all__ = (
     "Ldap",
     "Pki",
     "RabbitMQ",
+    "Ssh",
     "AsyncSecretsEngines",
 )
 
